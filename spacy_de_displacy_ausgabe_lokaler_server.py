8144630d3a06810c5bfc479856283ace2c0167e4
Verena702 <84783944+Verena702@users.noreply.github.com>
2022-04-04 10:54:43 +0200

    Update spacy_de_displacy_ausgabe_lokaler_server.py

--- spacy_de_displacy_ausgabe_lokaler_server.py
+++ spacy_de_displacy_ausgabe_lokaler_server.py
@@ -2,7 +2,7 @@ import spacy
 from spacy import displacy
 from bs4 import BeautifulSoup
 import requests
-#nlp = spacy.load("de_core_news_lg") Versuch, ob mehr Ergebnisse mit lg-Pipeline zustande kommen. Ergebnis: mehr Ergebnisse mit sm-Pipeline.
+
 nlp = spacy.load("de_core_news_sm")
 URL = "https://serdareviceportfolioit3.wordpress.com/ruby-portfolio/"
 page = requests.get(URL)
@@ -16,5 +16,4 @@ doc = nlp(result)
 
 #Ausgabe wird in lokalen Server mit farblicher Markierung der Wörter angezeigt
 sentence_spans = list(doc.sents)
-#displacy.serve(sentence_spans, style="dep")
 displacy.serve(doc, style="ent")
